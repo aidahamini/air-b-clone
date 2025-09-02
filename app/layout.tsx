1a45c16dd2f5d16837452efff06eea05a8a4060d
Aida <aidahamini03@gmail.com>
2025-09-02 17:59:30 +0100

    new edition

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
+  title: 'Air-B',
+  description: 'Air-B Clone',
 }
 
 const font = Nunito({ 
